71fbb61c4321fec1e911c32dc5a7534a6673a2a7
Cody Justus <codydj13@hotmail.com>
2021-11-01 18:11:22 -0600

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  // MUST be called directly inside component functions
+  // const [title, setTitle] = useState(props.title);
+
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+}
+
+export default ExpenseItem;
